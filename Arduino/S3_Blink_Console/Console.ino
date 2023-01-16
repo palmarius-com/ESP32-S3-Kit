50b0c617ebdc5d860637f446c8b529ba601b375f
Palmarius <collin.sale@icloud.com>
2023-01-16 02:32:38 +0100

    firmware
    Added Pre-Programmed Firmware

---
+++ Arduino/S3_Blink_Console/Console.ino
@@ -0,0 +1,51 @@
+
+const byte numChars = 20;
+char receivedChars[numChars]; // an array to store the received data
+
+String inString = "";  // inString.toInt()
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////                                                        Console Read Characters   //////
+//////////////////////////////////////////////////////////////////////////////////////////////
+void recvWithEndMarker() 
+{
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;
+ 
+  while (Serial.available() > 0 && newConsoleData == false) 
+  {
+    rc = Serial.read();
+    
+    if (rc != endMarker) 
+    {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= numChars) 
+      {
+        ndx = numChars - 1;
+      }
+    }
+    else 
+    {
+      receivedChars[ndx] = '\0'; // terminate the string
+      inString = receivedChars;
+      ndx = 0;
+      newConsoleData = true;
+    }
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//////                                                        Console Handle Inputs     //////
+//////////////////////////////////////////////////////////////////////////////////////////////
+void handleConsoleInput() // running in main-loop !!
+{
+  recvWithEndMarker();
+  if (newConsoleData == true) 
+  {
+    Serial << "received String: '" << inString << "'" << endl;
+    pinNumber = inString.toInt();
+    newConsoleData = false;
+  }
+}
